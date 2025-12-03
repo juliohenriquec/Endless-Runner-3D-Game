4caab14d354f9f83d997255e55f9147990194336
Julio Henrique <juliohm37@outlook.com>
2025-12-03 09:23:46 -0300

    mpv

--- systems/global.gd
+++ systems/global.gd
@@ -1,5 +1,6 @@
 extends Node
 
 var coins := 0
-var uv_speed = 0.9
-var global_speed = uv_speed * 10
+var uv_speed = 2
+var global_speed: float = uv_speed * 10
+var distance: float = 0.0
--- systems/spawner.gd
+++ systems/spawner.gd
@@ -1,11 +1,40 @@
 extends Node3D
 
+@export var obstacle_scene: PackedScene  = load("res://entities/obstacle.tscn")
+@export var coin_scene: PackedScene = load("res://entities/coin.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+const LANES := [-2.0, 0.0, 2.0]
 
+@export var spawn_z_position: float = 40.0
+@export var spawn_interval: float = 1.2
+
+var timer := 0.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	timer -= delta
+	if timer <= 0:
+		spawn_random_object()
+		timer = spawn_interval
+		
+
+func spawn_random_object():
+	var lane = LANES[randi() % LANES.size()]
+	
+	var is_obstacle = randf() < 0.6
+	var instance
+	
+	if is_obstacle:
+		instance = obstacle_scene.instantiate()
+	else:
+		instance = coin_scene.instantiate()
+		
+	instance.position.x = lane
+	instance.position.z = spawn_z_position
+	
+	add_child(instance)
+	
+	
+	
+	
+	
